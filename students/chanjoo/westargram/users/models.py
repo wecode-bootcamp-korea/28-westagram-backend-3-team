75b6450a5d332d0549f83d4279c56227de8af32d
Chanjoo lee <dlckswn14@gmail.com>
2021-12-16 20:25:07 +0900

    user 모델링

---
+++ students/chanjoo/westargram/users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=300, unique=True)
+    password = models.CharField(max_length=1000)
+    phone_number = models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'users'
