bed8a2a76701d7401c67e70d747b47b334df00ac
Chanjoo lee <dlckswn14@gmail.com>
2021-12-16 20:44:23 +0900

    coding convention

--- students/chanjoo/westargram/users/models.py
+++ students/chanjoo/westargram/users/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=300, unique=True)
-    password = models.CharField(max_length=1000)
+    name         = models.CharField(max_length=50)
+    email        = models.CharField(max_length=300, unique=True)
+    password     = models.CharField(max_length=1000)
     phone_number = models.CharField(max_length=30)
 
     class Meta:
