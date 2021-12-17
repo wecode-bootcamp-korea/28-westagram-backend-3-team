09565c9675174abb902940fdc17bb3fb12624b91
Byeongyeon Jung <byeongyeonjung@Byeongyeonui-MacBookPro.local>
2021-12-17 16:54:31 +0900

    Edit: models.py

--- students/byeongyeon/westagram/users/models.py
+++ students/byeongyeon/westagram/users/models.py
@@ -5,12 +5,15 @@ from django.db import models
 
 
 class User(models.Model):
-    name     = models.CharField(max_length=50)
-    email    = models.CharField(max_length=100, unique=True)
-    password = models.CharField(max_length=50)
-    mobile   = models.PositiveIntegerField(max_length=20)
-    age      = models.PositiveIntegerField()
-    address  = models.CharField(max_length=500)             #질문사항 : 주소 정도의 텍스트 정보는 TextField 사용이 적합한지?
+    name       = models.CharField(max_length=50)
+    email      = models.CharField(max_length=100, unique=True)
+    password   = models.CharField(max_length=256)             #암호화를 대비한 max_length 설정
+    mobile     = models.PositiveIntegerField(max_length=20)
+    age        = models.PositiveIntegerField()
+    address    = models.CharField(max_length=500)
+    creaetd_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
     class Meta:
         db_table = 'users'
