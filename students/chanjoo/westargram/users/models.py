b6f7ce746c6e9fa2fc62c6303fd8738ed4e27e50
Chanjoo lee <dlckswn14@gmail.com>
2021-12-17 09:58:17 +0900

    user 모델에 필드 추가

--- students/chanjoo/westargram/users/models.py
+++ students/chanjoo/westargram/users/models.py
@@ -1,10 +1,16 @@
 from django.db import models
 
 class User(models.Model):
-    name         = models.CharField(max_length=50)
-    email        = models.CharField(max_length=300, unique=True)
-    password     = models.CharField(max_length=1000)
-    phone_number = models.CharField(max_length=30)
+    name          = models.CharField(max_length=50)
+    email         = models.EmailField(max_length=200, unique=True, null=True)
+    password      = models.CharField(max_length=1000)
+    phone_number  = models.CharField(max_length=30, unique=True, null=True)
+    username      = models.CharField(max_length=100, default='')
+    date_of_birth = models.DateField(null=True)
+    profile_photo = models.CharField(max_length=400, null=True)
+    website       = models.URLField(null=True)
+    introduction  = models.TextField(blank=True)
+    gender        = models.CharField(max_length=20, null=True)
 
     class Meta:
         db_table = 'users'
