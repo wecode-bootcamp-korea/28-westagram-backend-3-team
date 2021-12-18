620afb7642a036badece97f910d240185e10b9d4
sxxk2 <sxxklee@gmail.com>
2021-12-18 17:11:49 +0900

    edited models setting 2

--- students/jeongseoklee/westagram/users/models.py
+++ students/jeongseoklee/westagram/users/models.py
@@ -6,6 +6,8 @@ class User(models.Model):
     username  = models.CharField(max_length=40)
     user_id   = models.CharField(max_length=50, unique=True)
     password  = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table: 'users'
