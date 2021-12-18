1acd536285fe248412c28f2d6e19059eebc617cf
Chanjoo lee <dlckswn14@gmail.com>
2021-12-18 19:47:47 +0900

    회원가입 방식 별로 validation 분리

--- students/chanjoo/westargram/users/views.py
+++ students/chanjoo/westargram/users/views.py
@@ -5,26 +5,42 @@ from django.views           import View
 from django.core.exceptions import ValidationError
 
 from users.models      import User
-from users.validations import validate_password, validate_phone
+from users.validations import validate_email, validate_password, validate_phone
 
 class SignUpView(View):
     def post(self, request):
         try:
             user_info = json.loads(request.body)
 
-            phone_number = user_info['phone_number']
+            email        = user_info.get('email')
+            phone_number = user_info.get('phone_number')
             name         = user_info['name']
             username     = user_info['username']
             password     = user_info['password']
 
-            validate_phone(phone_number)
+            # email로 가입한 경우
+            if email:
+                validate_email(email)
+
+                if User.objects.filter(email=email).exists():
+                    raise ValidationError('DUPLICATED_EMAIL')
+
+            # phone_number로 가입한 경우
+            if phone_number:
+                validate_phone(phone_number)
+
+                if User.objects.filter(phone_number=phone_number).exists():
+                    raise ValidationError('DUPLICATED_PHONE_NUMBER')
+
+            # 공통 validation
             validate_password(password)
 
-            if User.objects.filter(phone_number=phone_number).exists():
-                raise ValidationError('INVALID_PHONE_NUMBER')
+            if User.objects.filter(username=username).exists():
+                raise ValidationError('DUPLICATED_USERNAME')
 
             User.objects.create(
                 phone_number = phone_number,
+                email        = email,
                 name         = name,
                 username     = username,
                 password     = password
