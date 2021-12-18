9a08af1a8c8aec04d3b691fcbd34342b5249add6
sxxk2 <sxxklee@gmail.com>
2021-12-18 15:50:17 +0900

    edited models setting

--- students/jeongseoklee/westagram/users/models.py
+++ students/jeongseoklee/westagram/users/models.py
@@ -4,7 +4,7 @@ class User(models.Model):
     mobile    = models.CharField(max_length=30)
     email     = models.CharField(max_length=100, unique=True)
     username  = models.CharField(max_length=40)
-    id        = models.CharField(max_length=50, unique=True)
+    user_id   = models.CharField(max_length=50, unique=True)
     password  = models.CharField(max_length=256)
 
     class Meta:
