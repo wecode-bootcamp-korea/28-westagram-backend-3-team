82ca0747bcabe1a9d20a6d7a5c8bf97dde9e35ce
이민석 <minxi@iminseogs-MacBook-Air.local>
2021-12-18 20:21:31 +0900

    Add: westagram modeling Review 1

--- students/minseok/westagram/users/models.py
+++ students/minseok/westagram/users/models.py
@@ -3,10 +3,12 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name            = models.CharField(max_length=50)
-    email           = models.EmailField(max_length=128)
+    email           = models.EmailField(max_length=128, null=True)
     password        = models.CharField(max_length=256)
-    phone_number    = models.IntegerField()
+    phone_number    = models.CharField(max_length=100)
     user_name       = models.CharField(max_length=50)
-    website         = models.CharField(max_length=300)
-    introduce       = models.CharField(max_length=100)
-    profile_photo   = models.ImageField()
+    website         = models.CharField(max_length=1000, null=True)
+    introduce       = models.CharField(max_length=100, null=True)
+    profile_image   = models.CharField(max_length=1000, null=True)
+    created_at      = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
--- students/minseok/westagram/users/views.py
+++ students/minseok/westagram/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# import json
 
-# Create your views here.
+# from django.shortcuts import render
+# from django.http import JsonResponse
+# from django.views import View
+# from users.models import User
+
+# # Create your views here.
+
+# class UserView(View):
+#     def post(self, request):
+#         data = json.loads(request.body)
+#         User.objects.create(
+#             name = data['name'],
+#             email = data['email'],
+#             password = data['password'],
+#             phone_number = data['phone_number'],
+#             user_name = data['user_name'],
+#             website = data['website'],
+#             introduce = data['introduce'],
+#             photo_profile = data['photo_profile']
+#         )
+
+#         return JsonResponse({'message' : 'SUCCESS'}, status = 201)
+
+#     def get(self, request):
+#         users = User.objects.all()
+#         results = []
+
+#         for user in users:
