d64ed7f9d5abf63bbd707c6d8bd5567faabafa58
이민석 <minxi@iminseogs-MacBook-Air.local>
2021-12-19 15:15:46 +0900

    Add: westagram modeling Review 2

--- students/minseok/westagram/users/models.py
+++ students/minseok/westagram/users/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-# Create your models here.
 class User(models.Model):
     name            = models.CharField(max_length=50)
-    email           = models.EmailField(max_length=128, null=True)
+    email           = models.CharField(max_length=128)
     password        = models.CharField(max_length=256)
     phone_number    = models.CharField(max_length=100)
     user_name       = models.CharField(max_length=50)
-    website         = models.CharField(max_length=1000, null=True)
+    website_url     = models.CharField(max_length=1000, null=True)
     introduce       = models.CharField(max_length=100, null=True)
     profile_image   = models.CharField(max_length=1000, null=True)
     created_at      = models.DateTimeField(auto_now_add=True)
