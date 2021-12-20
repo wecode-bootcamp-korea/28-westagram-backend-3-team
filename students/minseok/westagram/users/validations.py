160320eb6a231e576c53b678b2f24ff535bae3fe
이민석 <minxi@iminseogs-MacBook-Air.local>
2021-12-20 21:34:57 +0900

    Add: westagram Signup second

--- students/minseok/westagram/users/validations.py
+++ students/minseok/westagram/users/validations.py
@@ -2,37 +2,20 @@ import re
 
 from django.core.exceptions import ValidationError
 
-def validation_email(value):
-    regex_email = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]\b"
+def validation_email(email):
+    regex_email = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 
-    if re.match(regex_email, value):
-        return value
+    if not re.match(regex_email, email):
+        raise ValidationError('Invalid email')
 
-    if not (re.match(regex_email, value)):
-        raise ValidationError(
-            ('%(value)s is invalid email'),
-            params={'value': value}
-        )
-def validation_password(value):
-    regex_password = r"[\w`~!@#$%^&*(),<.>/?]{8,}"
+def validation_password(password):
+    regex_password = "[\w`~!@#$%^&*(),<.>/?]{8,}"
     
-    if re.match(regex_password, value):
-        return value
+    if not re.match(regex_password, password):
+        raise ValidationError('Invalid password')
 
-    if not re.match(regex_password, value):
-        raise ValidationError(
-            ('%(value)s is invalid password'),
-            params={'value': value}
-        )
+def validation_phone_number(phone_number):
+    regex_phone_number = "\d{3}[- .]\d{3,4}[- .]\d{4}"
 
-def validation_phone_number(value):
-    regex_phone_number = r"\d{3}[- .]\d{3,4}[- .]\d{4}"
-
-    if re.match(regex_phone_number, value):
-        return value
-
-    if not (re.match(regex_phone_number, value)):
-        raise ValidationError(
-            ('%(value)s is invalid phone_number'),
-            params={'value': value}
-        )
+    if not (re.match(regex_phone_number, phone_number)):
+        raise ValidationError('Invalid phone_number')
--- students/minseok/westagram/users/views.py
+++ students/minseok/westagram/users/views.py
@@ -7,32 +7,36 @@ from django.views import View
 from django.core.exceptions import ValidationError
 
 from users.models import User
-from users.validations import validation_email, validation_password, validation_phone_number
+from users.validations import validation_email, validation_password
 
 class SignupView(View):
     def post(self, request):
         data = json.loads(request.body)
-
+        
         try:
-            if not validation_email(User.email):
-                return JsonResponse({"message" : ""}, status=400)
+            name = data['name']
+            email = data['email']
+            password = data['password']
+            phone_number = data['phone_number']
+            user_name = data['user_name']
 
-            if not validation_password(User.password):
-                return JsonResponse({"message" : "KEY_ERROR"}, status=400)
-
-            if User.objects.filter('email').exists():
+            validation_email(email)
+            validation_password(password)
+            
+            if User.objects.filter(email=email).exists():
                 return JsonResponse({"message" : "exists email"}, status=400)
 
             User.objects.create(
-                name = data['name'],
-                email = data['email'],
-                password = data['password'],
-                phone_number = data['phone_number'],
-                user_name = data['user_name']
+                name = name,
+                email = email,
+                password = password,
+                phone_number = phone_number,
+                user_name = user_name
             )
-
-        except ValidationError:
-            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
-
-        else:
             return JsonResponse({"message" : "SUSSESS"}, status=201)
+
+        except ValidationError as e:
+            return JsonResponse({"message" : e.message}, status=400)
+        
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
