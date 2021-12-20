82c38df916ddafaf48b84675d724cb0c34f9f4b2
sxxk2 <sxxklee@gmail.com>
2021-12-20 21:46:06 +0900

    edited models setting 3

--- students/jeongseoklee/westagram/users/models.py
+++ students/jeongseoklee/westagram/users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class User(models.Model):
-    mobile    = models.CharField(max_length=30)
-    email     = models.CharField(max_length=100, unique=True)
-    username  = models.CharField(max_length=40)
-    user_id   = models.CharField(max_length=50, unique=True)
-    password  = models.CharField(max_length=256)
+    mobile     = models.CharField(max_length=30)
+    email      = models.CharField(max_length=100, unique=True)
+    username   = models.CharField(max_length=40)
+    user_id    = models.CharField(max_length=50, unique=True)
+    password   = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
