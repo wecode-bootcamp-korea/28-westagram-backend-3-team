58b5c802f594d23dd9d8b4b8c884969531b982d9
Chanjoo lee <dlckswn14@gmail.com>
2021-12-20 14:13:24 +0900

    회원 가입 로직 수정

--- students/chanjoo/westargram/users/validations.py
+++ students/chanjoo/westargram/users/validations.py
@@ -3,15 +3,14 @@ import re
 def is_valid_email(email):
     REGEX_EMAIL = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 
-    # 일치한 부분을 리턴한다, 일치 하지 않으면 nan을 리턴한다...!
-    return bool(re.match(REGEX_EMAIL, email)) # bool 함수의 동작은 python의 특징이다
+    return re.match(REGEX_EMAIL, email)
 
 def is_valid_password(password):
     REGEX_PASSWORD = '^(?=.*[a-zA-Z])(?=.*[!@#$%^~*+=-])(?=.*[0-9]).{8,}$'
 
-    return bool(re.match(REGEX_PASSWORD, password))
+    return re.match(REGEX_PASSWORD, password)
 
 def is_valid_phone_number(phone_number):
     REGEX_PHONE = '^\d{3}-\d{3,4}-\d{4}$'
 
-    return bool(re.match(REGEX_PHONE, phone_number))
+    return re.match(REGEX_PHONE, phone_number)
--- students/chanjoo/westargram/users/views.py
+++ students/chanjoo/westargram/users/views.py
@@ -3,78 +3,48 @@ from json.decoder import JSONDecodeError
 
 from django.http            import JsonResponse
 from django.views           import View
-from django.core.exceptions import BadRequest
+from django.core.exceptions import ValidationError
 
 from users.models      import User
-from users.validations import is_valid_password, is_valid_email, is_valid_phone_number
-
-
-# Error codes
-#: 중복된 이메일
-DUPLICATED_EMAIL        = 'DUPLICATED_EMAIL'
-#: 중복된 전화번호
-DUPLICATED_PHONE_NUMBER = 'DUPLICATED_PHONE_NUMBER'
-#: 중복된 유저아이디
-DUPLICATED_USERNAME     = 'DUPLICATED_USERNAME'
-#: 잘못된 이메일
-INVALID_EMAIL           = 'INVALID_EMAIL'
-#: 잘못된 JSON 요청
-INVALID_JSON            = 'INVALID_JSON'
-#: 잘못된 패스워드
-INVALID_PASSWORD        = 'INVALID_PASSWORD'
-#: 잘못된 전화번호
-INVALID_PHONE_NUMBER    = 'INVALID_PHONE_NUMBER'
-#: 인가되지 않은 사용자
-INVALID_USER            = 'INVALID_USER'
-#: 필수 필드가 입력되지 않음
-MISSING_REQUIRED        = 'MISSING_REQUIRED'
-#: 이메일 또는 전화번호 중 하나만 입력해야함
-TOO_MANY_IDENTIFIERS    = 'TOO_MANY_IDENTIFIERS'
+from users.validations import is_valid_password, is_valid_email
 
 
 class SignUpView(View):
     def post(self, request):
         try:
             user_info = json.loads(request.body)
-        except JSONDecodeError:
-            raise BadRequest(INVALID_JSON)
 
-        try:
-            email        = user_info.get('email')
-            phone_number = user_info.get('phone_number')
-            name         = user_info['name']
-            username     = user_info['username']
-            password     = user_info['password']
-        except KeyError:
-            raise BadRequest(MISSING_REQUIRED)
+            email    = user_info['email']
+            name     = user_info['name']
+            username = user_info['username']
+            password = user_info['password']
 
-        if not email and not phone_number:
-            raise BadRequest(MISSING_REQUIRED)
-        elif email and phone_number:
-            raise BadRequest(TOO_MANY_IDENTIFIERS)
-        elif email:
             if not is_valid_email(email):
-                raise BadRequest(INVALID_EMAIL)
+                raise ValidationError('INVALID_EMAIL')
+
+            if not is_valid_password(password):
+                raise ValidationError('INVALID_PASSWORD')
+
             if User.objects.filter(email=email).exists():
-                raise BadRequest(DUPLICATED_EMAIL)
-        else:
-            if not is_valid_phone_number(phone_number):
-                raise BadRequest(INVALID_PHONE_NUMBER)
-            if User.objects.filter(phone_number=phone_number).exists():
-                raise BadRequest(DUPLICATED_PHONE_NUMBER)
+                raise ValidationError('DUPLICATED_EMAIL')
 
-        if not is_valid_password(password):
-            raise BadRequest(INVALID_PASSWORD)
+            if User.objects.filter(username=username).exists():
+                raise ValidationError('DUPLICATED_USERNAME')
 
-        if User.objects.filter(username=username).exists():
-            raise BadRequest(DUPLICATED_USERNAME)
+            User.objects.create(
+                email    = email,
+                name     = name,
+                username = username,
+                password = password
+            )
 
-        User.objects.create(
-            phone_number = phone_number,
-            email        = email,
-            name         = name,
-            username     = username,
-            password     = password
-        )
+            return JsonResponse({'message':'CREATED'}, status=201)
 
-        return JsonResponse({'message':'CREATED'}, status=201)
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
+
+        except ValidationError as e:
+            return JsonResponse({'message':e.message}, status=400)
+
+        except JSONDecodeError:
+            return JsonResponse({'message':'INVALID_JSON'}, status=400)
