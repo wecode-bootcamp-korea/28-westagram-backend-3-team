be2b14a05d2382e4ac1bf743b7eacded134ebbfc
Chanjoo lee <dlckswn14@gmail.com>
2021-12-20 14:12:59 +0900

    이메일로만 회원가입하도록 모델 수정

--- students/chanjoo/westargram/users/models.py
+++ students/chanjoo/westargram/users/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class User(models.Model):
     name          = models.CharField(max_length=50)
     password      = models.CharField(max_length=1000)
-    phone_number  = models.CharField(max_length=30, unique=True, null=True)
+    phone_number  = models.CharField(max_length=30, null=True)
     username      = models.CharField(max_length=100, unique=True)
-    email         = models.EmailField(max_length=200, unique=True, null=True)
+    email         = models.EmailField(max_length=200, unique=True)
     date_of_birth = models.DateField(null=True)
     profile_photo = models.CharField(max_length=400, null=True)
     website       = models.URLField(null=True)
