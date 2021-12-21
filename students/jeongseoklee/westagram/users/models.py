9d53e573ab965facbb39b8219f59a1fcfe19cc02
sxxk2 <sxxklee@gmail.com>
2021-12-21 16:04:59 +0900

    3rd assignment check2

--- students/jeongseoklee/westagram/users/models.py
+++ students/jeongseoklee/westagram/users/models.py
@@ -4,7 +4,6 @@ class User(models.Model):
     mobile     = models.CharField(max_length=30)
     email      = models.CharField(max_length=100, unique=True)
     username   = models.CharField(max_length=40)
-    user_id    = models.CharField(max_length=50, unique=True)
     password   = models.CharField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- students/jeongseoklee/westagram/users/views.py
+++ students/jeongseoklee/westagram/users/views.py
@@ -7,11 +7,7 @@ from django.views import View
 
 from .models      import User
 
-
-# [a-zA-Z0-9._+-]+@[a-zA-Z0-9-]+/.[a-zA-Z0-9.]+
-# ‘^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$’
 email_validator    = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
-
 password_validator = re.compile('^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,}$')
 
 class SignUpView(View):
@@ -19,29 +15,25 @@ class SignUpView(View):
         data = json.loads(request.body)
 
         try:
+            email = data['email']
+            password = data['password']
+
             if not email_validator.match(data['email']):
                 return JsonResponse({"message" : "INVALID EMAIL"}, status=400)
 
-
-            #패스워드에 8자리이상, 문자, 숫자, 특수문자의 조합이 아니라면 에러
             if not password_validator.match(data['password']):
                 return JsonResponse({"message" : "INVALID PASSWORD"}, status=400)
 
-            
-            #이메일 중복체크, 중복시 에러 반환
-            if User.object.filter(email=email).exist():
+            if User.objects.filter(email=email).exists():
                 return JsonResponse({"message" : "EXISTED EMAIL"}, status=400)
 
-
-            #위 조건이 아닐시 (정상)
             User.objects.create(
+                email = email,
+                password = password,
                 mobile = data['mobile'],
-                email = data['email'],
                 username = data['username'],
-                user_id = data['user_id'],
-                password = data['password']
             )
             return JsonResponse({"message" : "SUCCESS"}, status=201)
-            
+
         except KeyError:
             return JsonResponse({"message" : "KEY-ERROR"}, status=400)
