8d6842fecf4158d6194409fb61bb64c75cc53ec4
sxxk2 <sxxklee@gmail.com>
2021-12-22 18:38:01 +0900

    4rd 2

--- students/jeongseoklee/westagram/users/urls.py
+++ students/jeongseoklee/westagram/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from users.views      import SignUpView
+from users.views      import SignUpView, LoginView
 
 urlpatterns = [
-	path('/signup', SignUpView.as_view())
+	path('/signup', SignUpView.as_view()),
+	path('/login',  LoginView.as_view())
 ]
--- students/jeongseoklee/westagram/users/views.py
+++ students/jeongseoklee/westagram/users/views.py
@@ -38,3 +38,21 @@ class SignUpView(View):
 
         except KeyError:
             return JsonResponse({"message" : "KEY-ERROR"}, status=400)
+
+
+class LoginView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+
+        try:
+            email    = data['email']
+            password = data['password']
+            
+            if not User.objects.filter(email=email).exists():
+                return JsonResponse({"message" : "INVALID EMAIL"}, status=401)
+
+            if User.objects.filter(email=email, password=password).exists():
+                return JsonResponse({"message" : "LOGIN SUCCESS"}, status=200)
+
+        except KeyError:
+            return JsonResponse({"message" : "KEY-ERROR"}, status=400)
